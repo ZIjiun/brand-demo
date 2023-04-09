c65a7a15f17a65fdd00f759cd883b800e538e755
Zijun <s410126080@gmail.com>
2023-04-09 09:16:52 +0800

    建立【查詢所有】功能，【數據訪問層】相關程式

--- src/main/java/com/Kent/mapper/BrandMapper.java
+++ src/main/java/com/Kent/mapper/BrandMapper.java
@@ -1,4 +1,18 @@
 package com.Kent.mapper;
 
+import com.Kent.pojo.Brand;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface BrandMapper {
+
+    /**
+     * 查詢所有
+     * @return
+     */
+    @Select("select * from tb_brand;")
+    @ResultMap("brandResultMap")
+    List<Brand> selectAll();
 }
