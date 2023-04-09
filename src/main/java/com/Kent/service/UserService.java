d934fda925e7495d2199cc68a59875d31f6e19b8
Zijun <s410126080@gmail.com>
2023-04-09 23:03:42 +0800

    建立【使用者登錄】功能，【業務邏輯層】相關程式

---
+++ src/main/java/com/Kent/service/UserService.java
@@ -0,0 +1,31 @@
+package com.Kent.service;
+
+import com.Kent.mapper.UserMapper;
+import com.Kent.pojo.User;
+import com.Kent.util.SqlSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class UserService {
+    SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
+
+    /**
+     * 使用者登入
+     * @return
+     */
+    public User login(String username, String password){
+        // 2. 取得 SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 取得 BrandMapper
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        // 4. 調用方法
+        User user = mapper.select(username, password);
+
+        // 釋放資源
+        sqlSession.close();
+
+        return user;
+    }
+}
