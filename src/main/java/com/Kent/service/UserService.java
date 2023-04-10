5e21ca2b6539d91de342c9664740583042e6bee3
Zijun <s410126080@gmail.com>
2023-04-10 19:41:32 +0800

    建立【註冊功能】功能，【業務邏輯層】相關程式

--- src/main/java/com/Kent/service/UserService.java
+++ src/main/java/com/Kent/service/UserService.java
@@ -10,10 +10,10 @@ public class UserService {
     SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
 
     /**
-     * 使用者登入
+     * 使用者登入*
      * @return
      */
-    public User login(String username, String password){
+    public User login(String username, String password) {
         // 2. 取得 SqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
@@ -27,5 +27,33 @@ public class UserService {
         sqlSession.close();
 
         return user;
+    }
+
+    /**
+     * 使用者註冊
+     *
+     * @param user
+     * @return
+     */
+    public boolean register(User user) {
+        // 2. 取得 SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 取得 BrandMapper
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        // 4. 判斷使用者是否存在
+        User u = mapper.selectByUsername(user.getUsername());
+
+        if (u == null) {
+            // 使用者不存在，可以註冊
+            mapper.add(user);
+            sqlSession.commit();
+        }
+
+        // 釋放資源
+        sqlSession.close();
+
+        return u == null;
     }
 }
